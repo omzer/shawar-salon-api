9f8eaf71d29f5de1e363c945296332ddbbc98294
hamza-sabri5 <hamoz.mms@gmail.com>
2021-01-06 19:23:39 +0200

    creating a express app and attach some middlewares to it.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
+//import libraries
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import { adminRouter } from './Routs/admin';
 
-admin.initializeApp();
+//initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase);
 
-export const ShopStatus = functions.https.onRequest(async (request, response) => {
-    const data = await admin.firestore().doc('ShopInfo/Info').get();
-    response.send({ "data": data.data });
-});
+//initialize express server
+const app = express();
+
+// this line allows express to understand the comming requests as json objects
+app.use(express.json());
+
+// adding middleware to the express app
+app.use('/admin',adminRouter);
+
+
+//define google cloud function name
+export const webApi = functions.https.onRequest(app);
